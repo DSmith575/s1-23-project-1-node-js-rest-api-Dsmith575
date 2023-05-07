2f14237e2aca8d80347ffbae4c2785deff5592f4
DSmith575 <SMITDE5@student.op.ac.nz>
2023-05-07 21:11:49 +1200

    test(trying to get pagination tests working): tests commented out due to not working correctly

---
+++ test/07-characterCreating-test.js
@@ -0,0 +1,160 @@
+process.env.NODE_ENV = "testing";
+
+import chai from "chai";
+import chaiHttp from "chai-http";
+import { describe, it } from "mocha";
+
+import app from "../app.js";
+
+chai.use(chaiHttp);
+
+const BASE_URL = "/api";
+
+const character = {
+  name: "Abc",
+  affinity: "Light",
+  description: "Test",
+};
+
+const characterTwo = {
+  name: "Defg",
+  affinity: "Light",
+  description: "Test",
+};
+
+const characterThree = {
+  name: "hijk",
+  affinity: "Light",
+  description: "Test",
+};
+
+const characterFour = {
+  name: "lmnop",
+  affinity: "Light",
+  description: "Test",
+};
+
+const characterFive = {
+  name: "qrs",
+  affinity: "Light",
+  description: "Test",
+};
+
+const characterSix = {
+  name: "tuv",
+  affinity: "Light",
+  description: "Test",
+};
+
+const characterSeven = {
+  name: "wxyz",
+  affinity: "Light",
+  description: "Test",
+};
+
+describe("Character filling for pagination", () => {
+  it("should create a character", (done) => {
+    chai
+      .request(app)
+      .post(`${BASE_URL}/v1/characters`)
+      .send(character)
+      .end((__, characterRes) => {
+        chai.expect(characterRes.status).to.be.equal(201);
+        chai.expect(characterRes.body).to.be.a("object");
+        chai
+          .expect(characterRes.body.msg)
+          .to.be.equal("Character successfully created");
+        done();
+      });
+  });
+
+  it("should create a character", (done) => {
+    chai
+      .request(app)
+      .post(`${BASE_URL}/v1/characters`)
+      .send(characterTwo)
+      .end((__, characterRes) => {
+        chai.expect(characterRes.status).to.be.equal(201);
+        chai.expect(characterRes.body).to.be.a("object");
+        chai
+          .expect(characterRes.body.msg)
+          .to.be.equal("Character successfully created");
+        done();
+      });
+  });
+
+  it("should create a character", (done) => {
+    chai
+      .request(app)
+      .post(`${BASE_URL}/v1/characters`)
+      .send(characterThree)
+      .end((__, characterRes) => {
+        chai.expect(characterRes.status).to.be.equal(201);
+        chai.expect(characterRes.body).to.be.a("object");
+        chai
+          .expect(characterRes.body.msg)
+          .to.be.equal("Character successfully created");
+        done();
+      });
+  });
+
+  it("should create a character", (done) => {
+    chai
+      .request(app)
+      .post(`${BASE_URL}/v1/characters`)
+      .send(characterFour)
+      .end((__, characterRes) => {
+        chai.expect(characterRes.status).to.be.equal(201);
+        chai.expect(characterRes.body).to.be.a("object");
+        chai
+          .expect(characterRes.body.msg)
+          .to.be.equal("Character successfully created");
+        done();
+      });
+  });
+
+  it("should create a character", (done) => {
+    chai
+      .request(app)
+      .post(`${BASE_URL}/v1/characters`)
+      .send(characterFive)
+      .end((__, characterRes) => {
+        chai.expect(characterRes.status).to.be.equal(201);
+        chai.expect(characterRes.body).to.be.a("object");
+        chai
+          .expect(characterRes.body.msg)
+          .to.be.equal("Character successfully created");
+        done();
+      });
+  });
+
+  it("should create a character", (done) => {
+    chai
+      .request(app)
+      .post(`${BASE_URL}/v1/characters`)
+      .send(characterSix)
+      .end((__, characterRes) => {
+        chai.expect(characterRes.status).to.be.equal(201);
+        chai.expect(characterRes.body).to.be.a("object");
+        chai
+          .expect(characterRes.body.msg)
+          .to.be.equal("Character successfully created");
+        done();
+      });
+  });
+
+  it("should create a character", (done) => {
+    chai
+      .request(app)
+      .post(`${BASE_URL}/v1/characters`)
+      .send(characterSeven)
+      .end((__, characterRes) => {
+        chai.expect(characterRes.status).to.be.equal(201);
+        chai.expect(characterRes.body).to.be.a("object");
+        chai
+          .expect(characterRes.body.msg)
+          .to.be.equal("Character successfully created");
+        done();
+      });
+  });
+});
